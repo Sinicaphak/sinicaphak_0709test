6c83c3d01c08555396b5ef85d83de04c59457fb0
廖俊煜 <2967306689@qq.com>
2025-07-09 17:38:57 +0800

    temp

--- temp_tool/compile_tool.sh
+++ temp_tool/compile_tool.sh
@@ -1,9 +1,9 @@
-cd "/home/phak/star_net/test0707/build"
+cd "/home/phak/star_net/sinicaphak_0709test/build"
 cmake ..
 make
 echo "编译完成"
 
-cd "/home/phak/star_net/test0707/output"
+cd "/home/phak/star_net/sinicaphak_0709test/output"
 cp ./main ./user_aaa
 cp ./main ./user_bbb
 echo "部署完成"
